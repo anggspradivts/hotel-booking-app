ae849f60092f0096769c8268bad4844275b4f764
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-09-05 12:00:29 +0800

    intall midtrans for payment gateway

--- app/(routes)/book/[propertyId]/payment/_components/payment-section.tsx
+++ app/(routes)/book/[propertyId]/payment/_components/payment-section.tsx
@@ -4,10 +4,14 @@ import { useSearchParams } from "next/navigation";
 const PaymentSectionPage = () => {
   const searchParams = useSearchParams();
   const paymentId = searchParams.get("paymentId");
-  
+  if (!paymentId) {
+    return <div>
+      payment not found
+    </div>
+  }
   return ( 
-    <div>
-      Halo
+    <div className="p-4 border border-indigo-100 rounded">
+      hallo
     </div>
    );
 }
