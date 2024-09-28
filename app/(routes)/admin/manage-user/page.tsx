be298a134b5741e05dab245ab990a47bd67f3a59
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-09-28 08:41:48 +0800

    test: test deployments to make sure the errors2

--- app/(routes)/admin/manage-user/page.tsx
+++ app/(routes)/admin/manage-user/page.tsx
@@ -1,9 +1,9 @@
 "use server"
-import { db } from "@/lib/db";
+// import { db } from "@/lib/db";
 import AdminLayout from "../Layout";
 
 const page = async () => {
-  const getUsers = await db.user.findMany();
+  // const getUsers = await db.user.findMany();
 
   return (
     <AdminLayout>
